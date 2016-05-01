4f6cef02b12a2aa68d1573f327f955106a722f11
AlexL70 <alexander.levinson.70@gmail.com>
2016-05-01 21:58:28 +0300

    Create Store controller

---
+++ MVCMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public string Index()
+        {
+            return "Hello from Store.Index()";
+        }
+
+        // GET: Store/Browse
+        public string Browse()
+        {
+            return "Hello from Store.Browse()";
+        }
+
+        // GET: Store/Details
+        public string Details()
+        {
+            return "Hello from Store.Details()";
+        }
+    }
+}
