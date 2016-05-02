e0a29f1d9b60b7c4df42a68f3326a86e9ebb35f1
AlexL70 <alexander.levinson.70@gmail.com>
2016-05-02 22:44:17 +0300

    Add Entity Framework. Configure DB access and initialization.

--- MVCMusicStore/Global.asax.cs
+++ MVCMusicStore/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +13,7 @@ namespace MVCMusicStore
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new MVCMusicStore.Models.SampleData());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
---
+++ MVCMusicStore/Models/MusicStoreEntities.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+
+namespace MVCMusicStore.Models
+{
+    public class MusicStoreEntities : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+    }
+}
