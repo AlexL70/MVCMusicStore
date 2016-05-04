c5534f62dcc9493b502c596324f92a7ec0c3d205
AlexL70 <alexander.levinson.70@gmail.com>
2016-05-04 21:36:18 +0300

    Data annotation added to Album; client side validation now works.

--- MVCMusicStore/Models/Album.cs
+++ MVCMusicStore/Models/Album.cs
@@ -1,14 +1,26 @@
-﻿namespace MVCMusicStore.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace MVCMusicStore.Models
 {
+    //[Bind(Exclude = nameof(Album.AlbumId))]
     public class Album
     {
+        [ScaffoldColumn(false)]
         public int AlbumId { get; set; }
+        [DisplayName("Genre")]
         public int GenreId { get; set; }
+        [DisplayName("Artist")]
         public int ArtistId { get; set; }
+        [Required(ErrorMessage = "An album title is required."), StringLength(160)]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Price is required."),
+            Range(0.01, 100, ErrorMessage = "Price must be between 0.01 and 100.")]
         public decimal Price { get; set; }
+        [DisplayName("Album Art URL"), StringLength(1024)]
         public string AlbumArtUrl { get; set; }
-        public Genre Genre { get; set; }
-        public Artist Artist { get; set; }
+        public virtual Genre Genre { get; set; }
+        public virtual Artist Artist { get; set; }
     }
 }
