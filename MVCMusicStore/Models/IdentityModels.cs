6ae736f56ba48c24c33c35699108df1541b5717d
AlexL70 <alexander.levinson.70@gmail.com>
2016-05-10 20:59:39 +0300

    Now security objects use the same database as music store ones. Spare connection string for localDb has been created.

--- MVCMusicStore/Models/IdentityModels.cs
+++ MVCMusicStore/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace MVCMusicStore.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base(nameof(MusicStoreEntities), throwIfV1Schema: false)
         {
         }
 
