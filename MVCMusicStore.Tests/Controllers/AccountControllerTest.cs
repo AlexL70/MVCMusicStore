42a931e03b6171dcff9ab0cb02717a7ab05a3eaf
AlexL70 <alexander.levinson.70@gmail.com>
2016-06-09 13:57:05 +0300

    Add test class for AccountController

---
+++ MVCMusicStore.Tests/Controllers/AccountControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using MVCMusicStore.Controllers;
+using System.Web.Mvc;
+
+namespace MVCMusicStore.Tests.Controllers
+{
+    [TestFixture, Category(nameof(AccountController))]
+    public class AccountControllerTest
+    {
+        //private ApplicationDbContext context;
+        private AccountController controller;
+
+        public AccountControllerTest()
+        {
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            controller = new AccountController();
+        }
+
+        [Test]
+        public void LoginGet()
+        {
+            var result = controller.Login("") as ActionResult;
+            Assert.IsNotNull(result);
+        }
+    }
+}
